bcdb9215614973dfebd8b6a5ede2f667cf5af6a8
kaik <kaikfreitas23@gmail.com>
2025-09-24 22:26:15 -0300

    inserção de novas funções para o código

--- src/main.py
+++ src/main.py
@@ -7,14 +7,27 @@ def Options():
     if aux1 == 1:
         print('Total: ', total)
     elif aux1 == 2:
-        total += int(input('Digite a quantidade de pacotes que deseja inserir: '))
+        total += int(input('Digite a quantidade de caixas que deseja inserir: '))
+        size()
+        material()
     elif aux1 == 3:
-        total -= int(input('Digite a quantidade de pacotes que deseja retirar: '))
+        total -= int(input('Digite a quantidade de caixas que deseja retirar: '))
+
+
+def size():
+    length = float(input('Digite a comprimento da caixa(m): '))
+    width = float(input('Digite a largura da caixa(m): '))
+    height = float(input('Digite a altura da caixa(m): '))
+
+def material():
+    tipo = input('Insira o material da caixa: ')
+    return tipo
+
+
 
 while True:
-    print('1. Ver total armazenado\n2. Inserir pacotes \n3. Retirar pacote\n4. Sair')
+    print('1. Ver total armazenado\n2. Inserir caixas \n3. Retirar caixas\n4. Sair')
     aux1 = int(input('Escolha uma opção: ')) 
     if aux1 == 4: 
         break
-    Options()
-
+Options()
